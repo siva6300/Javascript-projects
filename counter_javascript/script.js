f9660a63612008ffee8756366614c25082cdf674
N.Siva Shankar <siva630029@gmail.com>
2026-01-29 10:29:45 +0530

    counterjs

---
+++ counter_javascript/script.js
@@ -0,0 +1,28 @@
+
+
+let count = 0;
+
+const display = document.getElementById('count');
+
+const add = document.getElementById('add');
+
+const sub = document.getElementById('sub');
+
+
+const reset = document.getElementById('reset');
+
+add.addEventListener('click', () => {
+    count ++ ;
+    display.textContent = count;
+})
+
+
+sub.addEventListener('click', () => {
+    count --;
+    display.textContent = count;
+})
+
+reset.addEventListener('click', () => {
+    count = 0;
+    display.textContent = count;
+})
